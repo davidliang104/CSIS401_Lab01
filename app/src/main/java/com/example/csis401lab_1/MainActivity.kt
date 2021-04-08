144b6cbb361ad809103b337480ba1f91d7cb2374
ragequitting101 <davidliang104@gmail.com>
2021-04-09 00:40:04 +0300

    Added some comments

--- app/src/main/java/com/example/csis401lab_1/MainActivity.kt
+++ app/src/main/java/com/example/csis401lab_1/MainActivity.kt
@@ -1,3 +1,8 @@
+/*
+    CSIS-401 Lab-01
+    by David Liang  (S00049751)
+ */
+
 package com.example.csis401lab_1
 
 import androidx.appcompat.app.AppCompatActivity
@@ -5,7 +10,6 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlin.properties.Delegates
 
 class MainActivity : AppCompatActivity() {
     var choice: Double? = null
@@ -15,6 +19,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun setSize(v: View) {
+        // Choose the size of the photos
        choice = when (v) {
             option1 -> 0.19
             option2 -> 0.49
@@ -24,7 +29,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun displayPrice(v:View) {
+        // Get number entered by user
         val num = numOfPrints.text.toString().toDoubleOrNull()
+        // Check if either the input is invalid, or if no size was selected
         if (num == null) {
             textResult.text = ""
             Toast.makeText(this, "Enter a valid number", Toast.LENGTH_SHORT).show()
